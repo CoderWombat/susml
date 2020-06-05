80d41afc6eea04fb1b3f7a4cef096fffebecdac2
Emanuel Metzenthin <emanuel.metzenthin@student.hpi.de>
2020-06-05 13:56:23 +0200

    Add distribution script

---
+++ distribute_pipeline.sh
@@ -0,0 +1,12 @@
+scp ./image-net-pipeline.py pi@10.42.0.81:~/susml/emanuel_tobias/
+scp ./image-net-pipeline.py pi@10.42.0.83:~/susml/emanuel_tobias/
+scp ./image-net-pipeline.py pi@10.42.0.92:~/susml/emanuel_tobias/
+scp ./image-net-pipeline.py pi@10.42.0.129:~/susml/emanuel_tobias/
+scp ./image-net-pipeline.py pi@10.42.0.137:~/susml/emanuel_tobias/
+scp ./image-net-pipeline.py pi@10.42.0.146:~/susml/emanuel_tobias/
+scp ./image-net-pipeline.py pi@10.42.0.176:~/susml/emanuel_tobias/
+scp ./image-net-pipeline.py pi@10.42.0.177:~/susml/emanuel_tobias/
+scp ./image-net-pipeline.py pi@10.42.0.197:~/susml/emanuel_tobias/
+scp ./image-net-pipeline.py pi@10.42.0.203:~/susml/emanuel_tobias/
+scp ./image-net-pipeline.py pi@10.42.0.216:~/susml/emanuel_tobias/
+scp ./image-net-pipeline.py pi@10.42.0.252:~/susml/emanuel_tobias/
