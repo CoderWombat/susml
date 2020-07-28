0fbc0bf3889738cf75ffb21d10bf3ea3d2031906
Emanuel Metzenthin <emanuel.metzenthin@student.hpi.de>
2020-07-28 15:23:54 +0200

    Distribution script

---
+++ distribute_dataset.sh
@@ -0,0 +1,14 @@
+scp -r ~/susml/emanuel_tobias/whole-TIN/tiny-imagenet/0/tiny-imagenet pi@pi01.local:~/susml/emanuel_tobias/tiny-imagenet
+scp -r ~/susml/emanuel_tobias/whole-TIN/tiny-imagenet/1/tiny-imagenet pi@pi02.local:~/susml/emanuel_tobias/tiny-imagenet
+scp -r ~/susml/emanuel_tobias/whole-TIN/tiny-imagenet/2/tiny-imagenet pi@pi03.local:~/susml/emanuel_tobias/tiny-imagenet
+scp -r ~/susml/emanuel_tobias/whole-TIN/tiny-imagenet/3/tiny-imagenet pi@pi04.local:~/susml/emanuel_tobias/tiny-imagenet
+scp -r ~/susml/emanuel_tobias/whole-TIN/tiny-imagenet/4/tiny-imagenet pi@pi05.local:~/susml/emanuel_tobias/tiny-imagenet
+scp -r ~/susml/emanuel_tobias/whole-TIN/tiny-imagenet/5/tiny-imagenet pi@pi06.local:~/susml/emanuel_tobias/tiny-imagenet
+scp -r ~/susml/emanuel_tobias/whole-TIN/tiny-imagenet/6/tiny-imagenet pi@pi07.local:~/susml/emanuel_tobias/tiny-imagenet
+scp -r ~/susml/emanuel_tobias/whole-TIN/tiny-imagenet/7/tiny-imagenet pi@pi08.local:~/susml/emanuel_tobias/tiny-imagenet
+scp -r ~/susml/emanuel_tobias/whole-TIN/tiny-imagenet/8/tiny-imagenet pi@pi09.local:~/susml/emanuel_tobias/tiny-imagenet
+scp -r ~/susml/emanuel_tobias/whole-TIN/tiny-imagenet/9/tiny-imagenet pi@pi10.local:~/susml/emanuel_tobias/tiny-imagenet
+scp -r ~/susml/emanuel_tobias/whole-TIN/tiny-imagenet/10/tiny-imagenet pi@pi11.local:~/susml/emanuel_tobias/tiny-imagenet
+scp -r ~/susml/emanuel_tobias/whole-TIN/tiny-imagenet/11/tiny-imagenet pi@pi12.local:~/susml/emanuel_tobias/tiny-imagenet
+
+cp -r ~/susml/emanuel_tobias/small-val/tiny-imagenet/val ~/susml/emanuel_tobias/tiny-imagenet/val
