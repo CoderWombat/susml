7170a71c1514e15c0b6f1b25c566608eacef74d7
Emanuel Metzenthin <emanuel.metzenthin@student.hpi.de>
2020-07-29 13:33:30 +0200

    dist pipeline

--- distribute_pipeline.sh
+++ distribute_pipeline.sh
@@ -1,25 +1,12 @@
-#scp ./image-net-pipeline.py pi@10.42.0.81:~/susml/emanuel_tobias/
-#scp ./image-net-pipeline.py pi@10.42.0.83:~/susml/emanuel_tobias/
-#scp ./image-net-pipeline.py pi@10.42.0.92:~/susml/emanuel_tobias/
-#scp ./image-net-pipeline.py pi@10.42.0.129:~/susml/emanuel_tobias/
-#scp ./image-net-pipeline.py pi@10.42.0.137:~/susml/emanuel_tobias/
-#scp ./image-net-pipeline.py pi@10.42.0.146:~/susml/emanuel_tobias/
-#scp ./image-net-pipeline.py pi@10.42.0.176:~/susml/emanuel_tobias/
-#scp ./image-net-pipeline.py pi@10.42.0.177:~/susml/emanuel_tobias/
-#scp ./image-net-pipeline.py pi@10.42.0.197:~/susml/emanuel_tobias/
-#scp ./image-net-pipeline.py pi@10.42.0.203:~/susml/emanuel_tobias/
-#scp ./image-net-pipeline.py pi@10.42.0.216:~/susml/emanuel_tobias/
-#scp ./image-net-pipeline.py pi@10.42.0.252:~/susml/emanuel_tobias/
-
-scp ./image-net-pipeline.py pi@pi01.local:~/susml/emanuel_tobias/
-scp ./image-net-pipeline.py pi@pi02.local:~/susml/emanuel_tobias/
-scp ./image-net-pipeline.py pi@pi03.local:~/susml/emanuel_tobias/
-scp ./image-net-pipeline.py pi@pi04.local:~/susml/emanuel_tobias/
-scp ./image-net-pipeline.py pi@pi05.local:~/susml/emanuel_tobias/
-scp ./image-net-pipeline.py pi@pi06.local:~/susml/emanuel_tobias/
-scp ./image-net-pipeline.py pi@pi07.local:~/susml/emanuel_tobias/
-scp ./image-net-pipeline.py pi@pi08.local:~/susml/emanuel_tobias/
-scp ./image-net-pipeline.py pi@pi09.local:~/susml/emanuel_tobias/
-scp ./image-net-pipeline.py pi@pi10.local:~/susml/emanuel_tobias/
-scp ./image-net-pipeline.py pi@pi11.local:~/susml/emanuel_tobias/
-scp ./image-net-pipeline.py pi@pi12.local:~/susml/emanuel_tobias/
+scp -r src/ray pi@pi01.local:~/susml/emanuel_tobias/
+scp -r src/ray pi@pi02.local:~/susml/emanuel_tobias/
+scp -r src/ray pi@pi03.local:~/susml/emanuel_tobias/
+scp -r src/ray pi@pi04.local:~/susml/emanuel_tobias/
+scp -r src/ray pi@pi05.local:~/susml/emanuel_tobias/
+scp -r src/ray pi@pi06.local:~/susml/emanuel_tobias/
+scp -r src/ray pi@pi07.local:~/susml/emanuel_tobias/
+scp -r src/ray pi@pi08.local:~/susml/emanuel_tobias/
+scp -r src/ray pi@pi09.local:~/susml/emanuel_tobias/
+scp -r src/ray pi@pi10.local:~/susml/emanuel_tobias/
+scp -r src/ray pi@pi11.local:~/susml/emanuel_tobias/
+scp -r src/ray pi@pi12.local:~/susml/emanuel_tobias/
